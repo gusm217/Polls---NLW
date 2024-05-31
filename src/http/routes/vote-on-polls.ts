9b5e048b5445f1a23446076a4fd36c10c7067ff0
Gustavo Mendes <gusm217@gmail.com>
2024-05-31 13:04:39 -0300

    refact: change persisting condition to endpoint

--- src/http/routes/vote-on-polls.ts
+++ src/http/routes/vote-on-polls.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import z from "zod";
 import { randomUUID } from "crypto";
+import { prisma } from "../../lib /prisma";
 
 export async function voteOnPoll(app: FastifyInstance) {
 	app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -17,6 +18,28 @@ export async function voteOnPoll(app: FastifyInstance) {
 
 		let { sessionId } = request.cookies;
 
+		if (sessionId) {
+			const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+				where: {
+					sessionId_pollId: {
+						sessionId,
+						pollId
+					}
+				}
+			})
+
+		if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+			await prisma.vote.delete({
+				where: {
+					id: userPreviousVoteOnPoll.id
+				}
+			})
+
+		} else if(userPreviousVoteOnPoll) {
+			return reply.status(400).send({ message: `You've already voted on this poll.` })
+		}
+		}
+
 		if(!sessionId) {
 			sessionId = randomUUID();
 
@@ -28,6 +51,14 @@ export async function voteOnPoll(app: FastifyInstance) {
 			})
 		}
 
-		return reply.status(201).send({sessionId})
+		await prisma.vote.create({
+			data: {
+				sessionId,
+				pollId,
+				pollOptionId
+			}
+		})
+
+		return reply.status(201).send()
 	});
 }
