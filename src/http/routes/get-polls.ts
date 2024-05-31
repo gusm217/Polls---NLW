cdf616f58d398e7037ac6663a7eec7c6d3ec1336
Gustavo Mendes <gusm217@gmail.com>
2024-05-31 12:12:36 -0300

    feat: add getPoll endpoint

---
+++ src/http/routes/get-polls.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../../lib /prisma";
+import z from "zod";
+
+export async function getPoll(app: FastifyInstance) {
+	app.get('/polls/:pollId', async (request, reply) => {
+		const createPollBody = z.object({
+			pollId: z.string().uuid()
+		})
+
+		const { pollId } = createPollBody.parse(request.params);
+
+		const poll = await prisma.poll.findUnique({
+			where: {
+				id: pollId
+			},
+
+			include: {
+				options: {
+					select: {
+						id: true,
+						title: true
+					}
+				}
+			}
+		})
+
+		return reply.send(poll)
+	});
+}
