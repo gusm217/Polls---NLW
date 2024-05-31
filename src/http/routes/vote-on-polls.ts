3ceaeaeb56caf550e834c9febc812e4289e2417e
Gustavo Mendes <gusm217@gmail.com>
2024-05-31 12:42:35 -0300

    feat: add voteOnPoll endpoint and session cookie validation

---
+++ src/http/routes/vote-on-polls.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+import { randomUUID } from "crypto";
+
+export async function voteOnPoll(app: FastifyInstance) {
+	app.post('/polls/:pollId/votes', async (request, reply) => {
+		const voteOnPollBody = z.object({
+			pollOptionId: z.string().uuid()
+		})
+
+		const voteOnPollParams = z.object({
+			pollId: z.string().uuid()
+		})
+
+		const { pollOptionId } = voteOnPollBody.parse(request.body);
+		const { pollId } = voteOnPollParams.parse(request.params);
+
+		let { sessionId } = request.cookies;
+
+		if(!sessionId) {
+			sessionId = randomUUID();
+
+			reply.setCookie('sessionId', sessionId, {
+				path: '/',
+				maxAge: 60 * 60 * 24 * 30, //30 days
+				signed: true,
+				httpOnly: true
+			})
+		}
+
+		return reply.status(201).send({sessionId})
+	});
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,18 @@
 import fastify from 'fastify';
 import { createPoll } from './routes/create-polls';
 import { getPoll } from './routes/get-polls';
+import cookie from '@fastify/cookie';
+import { voteOnPoll } from './routes/vote-on-polls';
 
 const app = fastify();
+app.register(cookie, {
+	secret: "polls-app-nlw",
+	hook: 'onRequest',
+})
 
 app.register(createPoll);
-app.register(getPoll)
+app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
 	console.log('Server running on port 3333')
